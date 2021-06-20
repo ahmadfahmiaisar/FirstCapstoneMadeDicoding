9b2fb143f75bd7252e450b0dd95ac5aab5a86a49
Ahmad Fahmi Aisar <fahmiaisar99@gmail.com>
2021-06-21 02:51:15 +0700

    base activity

--- base/src/main/java/com/example/base/abstraction/BaseActivity.kt
+++ base/src/main/java/com/example/base/abstraction/BaseActivity.kt
@@ -1,4 +1,38 @@
 package com.example.base.abstraction
 
-class BaseActivity {
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B : ViewBinding, V : ViewModel> : AppCompatActivity() {
+
+    private var _binding: ViewBinding? = null
+    private lateinit var mViewModel: V
+
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: B
+        get() = _binding as B
+
+    protected val vm: V
+        get() = mViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = getViewBinding(layoutInflater)
+        setContentView(requireNotNull(_binding).root)
+
+        mViewModel = ViewModelProvider(this).get(getViewModelClass())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    abstract val getViewBinding: (LayoutInflater) -> B
+    abstract fun getViewModelClass(): Class<V>
 }
